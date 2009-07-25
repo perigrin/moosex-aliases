fbae3eea54a4b5ed86b64a58af5f2bda74f70bf5
Chris Prather <chris@prather.org>
2009-07-25 16:59:17 -0400

    initial commit

---
+++ lib/MooseX/Aliases.pm
@@ -0,0 +1,16 @@
+package MooseX::Aliases;
+use Moose();
+use Moose::Exporter;
+
+Moose::Exporter->setup_import_methods( with_caller => ['alias'], );
+
+sub alias {
+    my ( $caller, $orig, $alias ) = @_;
+    my $meta   = Class::MOP::class_of($caller);
+    my $method = $meta->find_method_by_name($orig);
+    confess "cannot find method $origin to alias" unless $method;
+    $meta->add_method( $alias => $method );
+}
+
+1;
+__END__
