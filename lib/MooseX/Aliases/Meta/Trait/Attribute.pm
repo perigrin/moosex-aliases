c9170e1ef53d57e7bccd7f392d7a89bdb9842b14
Jesse Luehrs <doy@tozt.net>
2009-07-25 16:32:18 -0500

    add attribute trait for aliasing accessors

---
+++ lib/MooseX/Aliases/Meta/Trait/Attribute.pm
@@ -0,0 +1,27 @@
+package MooseX::Aliases::Meta::Trait::Attribute;
+use Moose::Role;
+use Moose::Util::TypeConstraints;
+
+subtype 'MooseX::Aliases::ArrayRef', as 'ArrayRef[Str]';
+coerce  'MooseX::Aliases::ArrayRef', from 'Str', via { [ $_ ] };
+
+has alias => (
+    is         => 'ro',
+    isa        => 'MooseX::Aliases::ArrayRef',
+    auto_deref => 1,
+    coerce     => 1,
+);
+
+after install_accessors => sub {
+    my $self = shift;
+    for my $alias ($self->alias) {
+        MooseX::Aliases::alias(
+            $self->associated_class->name,
+            $self->get_read_method => $alias,
+        );
+    }
+};
+
+no Moose::Role;
+
+1;
