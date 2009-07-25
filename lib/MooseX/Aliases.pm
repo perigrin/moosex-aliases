e8b085c7fb0c9e06f1d189596c56851b8540f658
Chris Prather <chris@prather.org>
2009-07-25 17:25:32 -0400

    update Makefile and POD

--- lib/MooseX/Aliases.pm
+++ lib/MooseX/Aliases.pm
@@ -1,4 +1,7 @@
 package MooseX::Aliases;
+use strict;
+our $VERSION = '0.01';
+
 use Moose();
 use Moose::Exporter;
 
@@ -13,4 +16,54 @@ sub alias {
 }
 
 1;
+
 __END__
+
+=head1 NAME
+
+MooseX::Aliases - A class to ...
+
+=head1 VERSION
+
+This documentation refers to version 0.01.
+
+=head1 SYNOPSIS
+
+    package MyApp;
+    use Moose;
+    use MooseX::Aliases;
+    
+    sub foo { ... }
+    alias foo => 'bar';
+    
+    MyApp->new->bar;
+
+=head1 DESCRIPTION
+
+The MooseX::Aliases class implements ...
+
+=head1 SUBROUTINES / METHODS
+
+=head2 alias Str $original Str $alias
+
+Alias the method $original to the method $alias
+
+=head1 DEPENDENCIES
+
+Moose
+
+=head1 BUGS AND LIMITATIONS
+
+None known currently, please email the author if you find any.
+
+=head1 AUTHOR
+
+Chris Prather (chris@prather.org)
+
+=head1 LICENCE
+
+Copyright 2009 by Chris Prather.
+
+This software is free.  It is licensed under the same terms as Perl itself.
+
+=cut
