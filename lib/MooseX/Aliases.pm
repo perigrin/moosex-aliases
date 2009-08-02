9d76646ee414213cc4f806e4eff09531e48e38a0
Jesse Luehrs <doy@tozt.net>
2009-08-01 20:21:18 -0500

    oops, syntax error

--- lib/MooseX/Aliases.pm
+++ lib/MooseX/Aliases.pm
@@ -30,7 +30,7 @@ sub alias {
     my ( $caller, $orig, $alias ) = @_;
     my $meta   = Class::MOP::class_of($caller);
     my $method = $meta->find_method_by_name($orig);
-    Moose->throw_error "cannot find method $orig to alias" unless $method;
+    Moose->throw_error("cannot find method $orig to alias") unless $method;
     $meta->add_method(
         $alias => _get_method_metaclass($method)->wrap(
             $method,
