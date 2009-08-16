a61eec35e2519ee7c6aaafeb60a4b857ab06e495
Jesse Luehrs <doy@tozt.net>
2009-08-15 20:17:05 -0500

    document the traits

--- lib/MooseX/Aliases/Meta/Trait/Attribute.pm
+++ lib/MooseX/Aliases/Meta/Trait/Attribute.pm
@@ -3,6 +3,33 @@ use Moose::Role;
 use Moose::Util::TypeConstraints;
 Moose::Util::meta_attribute_alias 'Aliased';
 
+=head1 NAME
+
+MooseX::Aliases::Meta::Trait::Attribute - attribute metaclass trait for
+L<MooseX::Aliases>
+
+=head1 SYNOPSIS
+
+    package MyApp::Role;
+    use Moose::Role;
+    use MooseX::Aliases;
+
+    has this => (
+        isa   => 'Str',
+        is    => 'rw',
+        traits => [qw(Aliased)],
+        alias => 'that',
+    );
+
+=head1 DESCRIPTION
+
+This trait adds the C<alias> option to attribute creation. It is automatically
+applied to all attributes when C<use MooseX::Aliases;> is run, but must be
+explicitly applied in roles, due to issues with Moose's handling of attributes
+in roles.
+
+=cut
+
 subtype 'MooseX::Aliases::ArrayRef', as 'ArrayRef[Str]';
 coerce  'MooseX::Aliases::ArrayRef', from 'Str', via { [$_] };
 
@@ -30,5 +57,20 @@ after install_accessors => sub {
 };
 
 no Moose::Role;
+
+=head1 AUTHOR
+
+  Jesse Luehrs <doy at tozt dot net>
+
+  Chris Prather (chris@prather.org)
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
 
 1;
--- lib/MooseX/Aliases/Meta/Trait/Method.pm
+++ lib/MooseX/Aliases/Meta/Trait/Method.pm
@@ -1,11 +1,47 @@
 package MooseX::Aliases::Meta::Trait::Method;
 use Moose::Role;
 
+=head1 NAME
+
+MooseX::Aliases::Meta::Trait::Method - method metaclass trait for
+L<MooseX::Aliases>
+
+=head1 DESCRIPTION
+
+This trait adds an attribute to metaclasses of aliased methods, to track which method they were aliased from.
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+=head2 aliased_from
+
+Returns the name of the method that this method is an alias of.
+
+=cut
+
 has aliased_from => (
     is  => 'ro',
     isa => 'Str',
 );
 
 no Moose::Role;
+
+=head1 AUTHOR
+
+  Jesse Luehrs <doy at tozt dot net>
+
+  Chris Prather (chris@prather.org)
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
 
 1;
